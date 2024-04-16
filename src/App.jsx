1c44b1f35825e58bf36d4eca78c4d0dd136d08f4
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-04-16 21:04:38 +0300

    Proje ayağa kaldırıldı

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <a href='https://www.linkedin.com/in/toygunkip/' target='_blank' className="text-3xl font-bold hover:underline">
+      Hello world!
+    </a>
+  )
+}
+
+export default App
