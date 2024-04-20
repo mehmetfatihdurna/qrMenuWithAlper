0068a0b8b12737800bb8c4989af3311055341f94
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-04-21 01:09:51 +0300

    useSize React Hook added.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,27 @@
-import styles from "../assets/styles/Home.module.css"
+import { useState } from "react";
+import styles from "../assets/styles/Home.module.css";
+import useSize from "../libs/useSize";
 
 export default function Home() {
-  var width = window.innerWidth;
-  return (
-    <div className={styles.overlay}>
-      <img src="/media/menugenel.png" className={styles.backgroundImage} width={width} />
+  const windowSize = useSize();
 
-      <div className={styles.container}>
-        <a href="">
-          <img src="/media/biralar.png" className={styles.menuItem} />
-        </a>
+  if (windowSize) {
+    return (
+      <div className={styles.overlay}>
+        <img src="/media/menugenel.png" className={styles.backgroundImage} width={window.innerWidth} />
 
-        <img src="/media/anayemekler.png" className={styles.menuItem} />
-        <img src="/media/aperatifler.png" className={styles.menuItem} alt="" />
-        <img src="/media/alkolluicecekler.png" className={styles.menuItem} alt="" />
+        <div className={styles.container}>
+          <a href="">
+            <img src="/media/biralar.png" className={styles.menuItem} />
+          </a>
+
+          <img src="/media/anayemekler.png" className={styles.menuItem} />
+          <img src="/media/aperatifler.png" className={styles.menuItem} alt="" />
+          <img src="/media/alkolluicecekler.png" className={styles.menuItem} alt="" />
+        </div>
+
       </div>
+    )
+  }
 
-    </div>
-  )
 }
