e43c3844c74fb633dab10fa7e352d84c1d9491aa
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-05-01 21:20:00 +0300

    Framer kütüphanesi eklendi ve homepage e animasyonlar eklendi.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from "../assets/styles/Home.module.css";
 import useSize from "../libs/useSize";
+import Reveal from "./utils/Reveal";
 
 export default function Home() {
   const windowSize = useSize();
@@ -11,21 +12,30 @@ export default function Home() {
         <img src="/media/menugenel.png" className={styles.backgroundImage} width={window.innerWidth} />
 
         <div className={styles.container}>
-          <a href="/menu/contentbiralar">
-            <img src="/media/biralar.png" className={styles.menuItem} />
-          </a>
+          <Reveal>
+            <a href="/menu/contentbiralar">
+              <img src="/media/biralar.png" className={styles.menuItem} />
+            </a>
+          </Reveal>
 
-          <a href="/menu/contentanayemekler">
-            <img src="/media/anayemekler.png" className={styles.menuItem} />
-          </a>
+          <Reveal>
+            <a href="/menu/contentanayemekler">
+              <img src="/media/anayemekler.png" className={styles.menuItem} />
+            </a>
+          </Reveal>
 
-          <a href="/menu/contentaperatifler">
-            <img src="/media/aperatifler.png" className={styles.menuItem} alt="" />
-          </a>
+          <Reveal>
+            <a href="/menu/contentaperatifler">
+              <img src="/media/aperatifler.png" className={styles.menuItem} alt="" />
+            </a>
+          </Reveal>
 
-          <a href="/menu/contentalkolluicecekler">
-            <img src="/media/alkolluicecekler.png" className={styles.menuItem} alt="" />
-          </a>
+
+          <Reveal>
+            <a href="/menu/contentalkolluicecekler">
+              <img src="/media/alkolluicecekler.png" className={styles.menuItem} alt="" />
+            </a>
+          </Reveal>
 
         </div>
 
